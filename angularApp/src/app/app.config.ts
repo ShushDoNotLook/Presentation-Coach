f09db545b7f604912c55d2e624d32dcffe893f0b
ShushDoNotLook <yilongjq@gmail.com>
2025-12-02 19:23:42 -0800

    Fix asset paths and enable hash routing for GitHub Pages

--- angularApp/src/app/app.config.ts
+++ angularApp/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -13,7 +13,7 @@ import { withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
     importProvidersFrom(provideFirebaseApp(() => initializeApp({
